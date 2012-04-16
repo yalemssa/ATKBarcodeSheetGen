3a686a68ee06ee863590412ead5f23ac913d2fe2
Don Mennerich <domenn@gmail.com>
2012-04-16 10:24:33 -0400

    XML -> FO Broken

--- src/edu/yale/mssa/xml/FOGen.java
+++ src/edu/yale/mssa/xml/FOGen.java
@@ -5,25 +5,39 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.net.MalformedURLException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+import sun.java2d.SunGraphicsEnvironment.T1Filter;
 
 public class FOGen {
     
     private BufferedOutputStream outp;
-   
+    private final StreamSource xml;
+    private final StreamSource xslt;
+    private final StreamResult result;
     
-    FOGen(String xml, String xslt) throws FileNotFoundException, MalformedURLException {
-
+    FOGen(String xml, String xslt) throws FileNotFoundException, MalformedURLException, TransformerConfigurationException, TransformerException {
         outp = new BufferedOutputStream(new FileOutputStream(new File("testout.xml")));
-        
-        TransformerFactory tf = TransformerFactory.newInstance();
-        System.out.println(tf.getClass());
-
-
+        this.xml = new StreamSource(new File(xml));
+        this.xslt = new StreamSource(new File(xslt));
+        result = new StreamResult(outp);
+        transform();
     }
     
-    public static void main(String[] args) throws FileNotFoundException, MalformedURLException{
+    private void transform() throws TransformerConfigurationException, TransformerException {
+        TransformerFactory tf = TransformerFactory.newInstance();
+        Transformer transformer = tf.newTransformer(xslt);
+        transformer.transform(xml, result);
+    }
+    
+    public static void main(String[] args) throws FileNotFoundException, MalformedURLException, TransformerConfigurationException, TransformerException{
         FOGen f = new FOGen("barcodes.xml", "barcodes2xsl-fo.xsl");
     }
+
+
     
 }
